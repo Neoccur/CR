5789aa46165eb58a22c52a2d64bbe050a83dcf18
Oxyde2 <maxime.djomby@gmail.com>
2019-08-01 11:13:33 +0200

    Fixed JS syntax :gear:  Added 1 FIXME :bug:

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-function ScrollUp() {
+function scrollUp() {
     window.scrollTo(0, 0);
-}
+};
 
-function ScrollDown() {
+function scrollDown() {
     window.scrollTo(0, document.body.scrollHeight);
-}
+};
 
-function Forever() {
-    while (true){
-        var a = element.scrollTop;
-        var b = element.scrollHeight - element.clientHeight;
-        var c = a / b;
-        
-        if (c < 0.6){
-            document.getElementById('top').classList.add = 'active';
-            document.getElementById('bottom').classList.remove = 'active';
-        }
-        else {
-            document.getElementById('top').classList.remove = 'active';
-            document.getElementById('bottom').classList.add = 'active';
-        }
+function colorSwitching() {
+
+    let a = document.documentElement.scrollTop;
+    //let b = document.body.clientHeight
+    //let c = a / b;
+
+    //// FIXME: Classes aren't working
+    if (a > 1050){
+        document.getElementById('contact').classList.add = 'active';
+        document.getElementById('accueil').classList.remove = 'active';
     }
-}
+    else {
+        document.getElementById('accueil').classList.remove = 'active';
+        document.getElementById('contact').classList.add = 'active';
+    };
+    //console.log(a, document.getElementById('contact').classList.length, document.getElementById('accueil').classList.length)
+};
